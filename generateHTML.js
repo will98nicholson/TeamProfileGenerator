e4053ecadaa894bd2efa5136796f0d0130c5aef4
William Nicholson <will98nicholson@gmail.com>
2021-04-04 10:23:15 -0400

    generatehtml

--- generateHTML.js
+++ generateHTML.js
@@ -3,7 +3,9 @@
 
 function generateHTML(data) {
     return `
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" 
+    integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==" 
+    crossorigin="anonymous" />
     <head>
         <title>Team Profiles</title>
     </head>
