7d3cda7b3c7cc1e17edc2e46d0308bf76b78396d
William Nicholson <will98nicholson@gmail.com>
2021-04-06 10:21:42 -0400

    tests

---
+++ test/employee.test.js
@@ -0,0 +1,9 @@
+//const Manager = require('./employeeclasses/Manager.js');
+
+describe('Employee', () => {
+    describe('getName', () => {
+        it('should return employee enetered name', () => {
+            expect(new Manager('William', '11111', 'william1@office.com', '101').getName()).toBe('William')
+
+        })
+    })
