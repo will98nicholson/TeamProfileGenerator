7aa627f454c80b3c4057fd0c90741028dd6761dc
William Nicholson <will98nicholson@gmail.com>
2021-04-06 10:54:54 -0400

    function

--- generateHTML.js
+++ generateHTML.js
@@ -2,13 +2,43 @@
 
 
 function generateHTML(data) {
-    return `
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" 
+    function renderManager(manager) {
+        return `<div class="ui card">
+        <div class="content">
+          <div class="header">${manager.name}</div>
+          <div class="meta">
+            <span class="category">${manager.getRole()}</span>
+          </div>
+          <div class="description">
+           ${manager.email}
+          </div>
+        </div>
+        <div class="extra content">
+          <div class="right floated author">
+            ${manager.officenumber}
+          </div>
+        </div>
+      </div>`
+    }
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" 
     integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==" 
     crossorigin="anonymous" />
     <head>
         <title>Team Profiles</title>
     </head>
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>
+    
     
     `
 
