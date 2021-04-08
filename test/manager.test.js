969e49ff3d57ce3ce34ab26cddfd16ab10e5b83d
William Nicholson <will98nicholson@gmail.com>
2021-04-08 15:01:38 -0400

    fixed manager test

--- test/manager.test.js
+++ test/manager.test.js
@@ -3,23 +3,23 @@ const Employee = require('../employeeclasses/employee');
 describe('Employee', () => {
     describe('getName', () => {
         it('should return employee enetered name', () => {
-            expect(new Manager('William', '1', 'william@office', '101').getName()).toBe('William')
+            expect(new Employee('William', '1', 'william@office', '101').getName()).toBe('William')
 
         })
     })
     describe('getEmail', () => {
         it('should return employee email', () => {
-            expect(new Manager('William', '1', 'william@office', '101').getEmail()).toBe('william@office')
+            expect(new Employee('William', '1', 'william@office', '101').getEmail()).toBe('william@office')
         })
     })
     describe('getID', () => {
         it('should return employee ID', () => {
-            expect(new Manager('William', '1', 'william@office', '101').getID()).toBe('1')
+            expect(new Employee('William', '1', 'william@office', '101').getID()).toBe('1')
         })
     })
     describe('getRole', () => {
         it('should return employee role', () => {
-            expect(new Manager('William', '1', 'william@office', '101').getRole()).toBe('Manager')
+            expect(new Employee('William', '1', 'william@office', '101').getRole()).toBe('Employee')
         })
     })
 
